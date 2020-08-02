2ac0e391709a9101dd5cb8aca12c37ecbefbbca4
Eduardo Lujan <eduardo.lujan@mobicash.mx>
2020-08-02 14:11:24 -0500

    Initial commit

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -1,23 +1,24 @@
 import pytest
+import unittest
 
 from src.fizz_buzz import fizz_buzz
 
 
-def test_divisible_by_3():
-    """
-    Test if is divisible with 4
-    """
-    assert fizz_buzz(3) == 'fizz'
+class TestFizzBuzz(unittest.TestCase):
+    def test_divisible_by_3(self):
+        """
+        Test if is divisible with 4
+        """
+        self.assertEqual(fizz_buzz(3), 'fizz')
 
+    def test_divisible_by_5(self):
+        """
+        Test if is divisible by 5
+        """
+        self.assertEqual(fizz_buzz(5), 'buzz')
 
-def test_divisible_by_5():
-    """
-    Test if is divisible by 5
-    """
-    assert fizz_buzz(5) == 'buzz'
-
-def test_divisible_by_5_and_3():
-    """
-    Test if is divisible by 5 and 3
-    """
-    assert fizz_buzz(3*5) == 'fizzbuzz'
+    def test_divisible_by_5_and_3(self):
+        """
+        Test if is divisible by 5 and 3
+        """
+        self.assertEqual(fizz_buzz(3*5), 'fizzbuzz')
